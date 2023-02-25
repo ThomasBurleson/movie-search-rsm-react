6ceac2769dc009571614a0301a2cb3701a74234a
thomasburleson <thomasburleson@gmail.com>
2023-02-25 10:19:23 -0600

    lab(bookmark): add bookmark support

---
+++ src/data-access/movies/movies.bookmarks.ts
@@ -0,0 +1,94 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { MovieViewModel } from './movies.model';
+
+/**
+ * What are the possbile URL query params?
+ */
+const MOVIE_URL_PARAMS = ['searchBy', 'filterBy', 'page'];
+
+// ************************************************
+// Query Param Utils
+// ************************************************
+
+function toKeyVals(searchParams: any, allowedKeys?: string[]) {
+  const params: Record<string, any> = {};
+  searchParams.forEach((val: string, key: string) => {
+    const isAllowed = !allowedKeys || allowedKeys.includes(key);
+    if (isAllowed && !!val) params[key] = val;
+  });
+
+  return params;
+}
+
+/**
+ * Which params in the URL should be used to update the AppStore?
+ */
+export function extractQueryParams(location: Location & { state?: any }): Record<string, any> {
+  const { search } = location;
+  const searchParams = new URLSearchParams(search || '');
+  const queryParams = toKeyVals(searchParams, MOVIE_URL_PARAMS);
+
+  const { page } = queryParams;
+  // Coerce non-string values to appropriate types.
+  if (page) queryParams['page'] = parseInt(page, 10);
+
+  return queryParams;
+}
+
+/**
+ * Which keys in the store should be shown on the URL?
+ */
+export function buildQueryParams(storeParams: Record<string, any>, urlParams: URLSearchParams): Record<string, any> {
+  // Scan all allowed fields for valid values
+  // NOTE: if undefined store value, then remove from URL
+  MOVIE_URL_PARAMS.forEach((k: string) => {
+    const value = k !== 'page' ? storeParams[k] : storeParams['pagination']['currentPage'];
+
+    if (value) urlParams.set(k, value);
+    else urlParams.delete(k);
+  });
+
+  return urlParams;
+}
+
+// **********************************************************
+// Bookmark utils for bidi synchronization of URL to Store
+// **********************************************************
+
+/**
+ * Gather current values in the store and reflect those
+ * to show on the URL for bookmarking
+ */
+export function syncStoreToUrl(vm: MovieViewModel): MovieViewModel {
+  const { history, location } = window;
+  if (history && location) {
+    const { search, origin, pathname } = location;
+
+    const searchParams = new URLSearchParams(search);
+    const urlParams = buildQueryParams(vm, searchParams);
+    const newUrl = `${origin}${pathname}?${urlParams.toString()}`;
+
+    // update the bookmark (replace so back arrow will not change state)
+    history.replaceState({ path: newUrl }, '', newUrl);
+  }
+  return vm;
+}
+
+/**
+ * Using the History location object, gather expected query params
+ * and update the Store state
+ *
+ * NOTE: do as the 'store' is being initialized/created
+ */
+export function syncUrlToStore(vm: MovieViewModel): MovieViewModel {
+  if (window?.location) {
+    let { searchBy, filterBy, page } = extractQueryParams(window.location);
+
+    searchBy ||= vm.searchBy;
+    filterBy ||= vm.filterBy;
+    page ||= vm.pagination.currentPage;
+
+    vm.searchMovies(searchBy, page, filterBy);
+  }
+  return vm;
+}
--- src/data-access/movies/movies.hook.ts
+++ src/data-access/movies/movies.hook.ts
@@ -1,24 +1,15 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
+import { useEffect, useState } from 'react';
 
-import { initState, MovieViewModel, MovieGenreViewModel } from './movies.model';
 import { PAGES, GENRES } from './_tests_/_mocks_/movies.data';
+import { initState, MovieViewModel, MovieGenreViewModel } from './movies.model';
+import { syncStoreToUrl, syncUrlToStore } from './movies.bookmarks';
 
 // ************************************************************************
 // Service instances with MOCK data...
 // NOTE: Placeholders until Facade + Store are implemented!!
 // ************************************************************************
 
-const vm: MovieViewModel = {
-  ...initState(),
-  searchBy: 'dogs',
-  allMovies: PAGES[0].list,
-  filteredMovies: PAGES[0].list,
-  pagination: PAGES[0].pagination,
-  updateFilter: () => {},
-  searchMovies: () => {},
-  selectGenresById: () => {},
-  clearFilter: () => {},
-};
 
 const genres: MovieGenreViewModel = {
   list: GENRES.genres,
@@ -31,6 +22,60 @@ const genres: MovieGenreViewModel = {
   status: { value: 'initializing' },
 };
 
+/**
+ * Factory to build mock ViewModel
+ */
+function makeViewModel(): MovieViewModel {
+  const showPage = (page: number) => { vm.pagination.currentPage = page };
+  let vm = {
+    ...initState(),
+    searchBy: 'dogs',
+    allMovies: PAGES[0].list,
+    filteredMovies: PAGES[0].list,
+    pagination: { ...PAGES[0].pagination, showPage },
+    searchMovies: () => {},
+    updateFilter: () => {},
+    selectGenresById: () => {},
+    clearFilter: () => {},
+  };
+
+  /**
+   * Special case for app startup
+   */
+  const searchOnStartup = (searchBy: string, page?:number, filterBy?:string) => {
+    filterBy ||= vm.filterBy;
+    page ||= vm.pagination.currentPage;
+    searchBy ||= vm.searchBy;
+    vm = ({ ...vm, searchBy, filterBy, pagination: { ...vm.pagination, currentPage: page } });
+  };
+  syncUrlToStore({...vm, searchMovies: searchOnStartup}); 
+
+  return vm;
+}
+
+/**
+ * Enable the updates to the VM to trigger hook re-renders
+ * NOTE: only need since we are using mock data INSTEAD of a Reactive Store
+ */
+function onAPI(vm, setVM): MovieViewModel {
+  const searchMovies = (searchBy: string, page?:number, filterBy?:string) => {    
+    setVM(vm => {
+      filterBy ||= vm.filterBy;
+      page ||= vm.pagination.currentPage;
+
+      return ({ ...vm, searchBy, filterBy, pagination: { ...vm.pagination, currentPage: page } })
+    });
+  };
+  const updateFilter = (filterBy: string) => {
+    setVM(vm => ({ ...vm, filterBy }));
+  }
+  const showPage = (page: number) => { setVM( vm => {
+    return ({ ...vm, pagination: { ...vm.pagination, currentPage: page } })
+  })};
+  const pagination = { ...vm.pagination, showPage };
+
+  return {...vm, pagination, searchMovies, updateFilter};
+}
 // ************************************************************************
 // ************************************************************************
 
@@ -44,5 +89,10 @@ export type MovieFacadeResults = [MovieViewModel, MovieGenreViewModel];
  * @returns MovieViewModel
  */
 export function useMovieFacade(): MovieFacadeResults {
-  return [vm, genres];
+  const [vm, setVM] = useState(makeViewModel);
+  
+  useEffect(() => { syncUrlToStore(vm) }, []);    // update store from URL
+  useEffect(() => { syncStoreToUrl(vm) }, [vm]);  // update URL from store
+
+  return [ onAPI(vm, setVM), genres ];
 }
